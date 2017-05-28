c453f63292c5352e9f5b098479a9c0b27fe72757
oxisrock <franciscoaular18@gmail.com>
2017-05-27 20:38:30 -0430

    ya puedo logearme con mi cuenta google y es muy bueno y tambien extre el displayName y la imagen que este tiene en su correo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import firebase from 'firebase';
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user: user });
+    })
+  }
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log (`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+  handleLogout ()
+  {
+    firebase.auth().signOut()
+    .then(result => console.log (`${result.user.email} ha salido de la aplicacion`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+  renderLoginButton () {
+    // si el usuario esta logeado haga
+    if (this.state.user) {
+      return(
+        <div>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Bienvenido {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Salir</button>
+        </div>
+      );
+    }else {
+      return(
+      <button onClick={this.handleAuth}> Login con Google</button>
+      );
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Practica React 3</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { this.renderLoginButton() }
         </p>
       </div>
     );
